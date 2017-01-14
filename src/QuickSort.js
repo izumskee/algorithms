1675261c8db6b0c571bea5b047566f10c4734418
Izumskee <izum.ff@gmail.com>
2017-01-14 16:44:25 +0300

    Rewrite Quicksort with ES6 class

--- src/QuickSort.js
+++ src/QuickSort.js
@@ -1,32 +1,43 @@
-const Quicksort = (A, p, r) => {
-  if (r <= p) return;
-
-  let q = Partition(A, p, r);
-
-  Quicksort(A, p, q - 1);
-  Quicksort(A, q + 1, r);
-};
-
-const Partition = (A, p, r) => {
-  let x = A[r];
-  let i = p - 1;
-
-  for (let j = p; j <= r - 1; j++) {
-    if (A[j] <= x) {
-      i++;
-      swap(A, i, j);
-    }
+class Quicksort {
+  constructor(array) {
+    this.A = array;
   }
 
-  swap(A, i + 1, r);
+  _quicksort(A, p, r) {
+    if (r <= p) return;
 
-  return i + 1;
-};
+    let q = this._partition(A, p, r);
+    this._quicksort(A, p, q - 1);
+    this._quicksort(A, q + 1, r);
+  }
 
-const swap = (array, indexA, indexB) => {
-  const temp = array[indexA];
-  array[indexA] = array[indexB];
-  array[indexB] = temp;
-};
+  _partition(A, p, r) {
+    let x = A[r];
+    let i = p - 1;
+
+    for (let j = p; j <= r - 1; j++) {
+      if (A[j] <= x) {
+        i++;
+        this._swap(A, i, j);
+      }
+    }
+
+    this._swap(A, i + 1, r);
+
+    return i + 1;
+  }
+
+  _swap(array, indexA, indexB) {
+    const temp = array[indexA];
+    array[indexA] = array[indexB];
+    array[indexB] = temp;
+  }
+
+  sort() {
+    this._quicksort(this.A, 0, this.A.length - 1);
+    return this.A;
+  }
+
+}
 
 export default Quicksort;
--- test/Quicksort.spec.js
+++ test/Quicksort.spec.js
@@ -4,10 +4,9 @@ import { expect } from 'chai';
 describe('Quicksort testing', () => {
 
   it('sort array', () => {
-    let A = [3, 2, 8, 4, 11, 1];
-    Quicksort(A, 0, A.length - 1);
+    let res = new Quicksort([3, 2, 8, 4, 11, 1]).sort();
 
-    expect(A).to.deep.equal([1, 2, 3, 4, 8, 11]);
+    expect(res).to.deep.equal([1, 2, 3, 4, 8, 11]);
   });
 
 });
