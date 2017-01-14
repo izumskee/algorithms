e2af623e7303243dc7e795455db88a15392afa2b
Izumskee <izum.ff@gmail.com>
2017-01-14 16:47:23 +0300

    Delete unused file

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import Quicksort from './Quicksort';
-import shuffleArray from './shuffleArray';
-
-const A = [4, 8, 3, 9, 12, 15, 11, 1, 19, 17, 23];
-shuffleArray(A); // for performance
-Quicksort(A, 0, A.length - 1);
-
-console.log('Quicksort >', A);
