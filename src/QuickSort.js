cf6ef6136b9c436d39ac4210461d2189f75939b0
Izumskee <izum.ff@gmail.com>
2017-01-15 00:01:40 +0300

    Minor changes in Quicksort

--- src/QuickSort.js
+++ src/QuickSort.js
@@ -6,13 +6,21 @@ class Quicksort {
   _quicksort(A, p, r) {
     if (r <= p) return;
 
-    let q = this._partition(A, p, r);
+    const q = this._randomizedPartition(A, p, r);
     this._quicksort(A, p, q - 1);
     this._quicksort(A, q + 1, r);
   }
 
+  _randomizedPartition(A, p, r) {
+    const i = this._random(p, r);
+
+    this._swap(A, r, i);
+
+    return this._partition(A, p, r);
+  }
+
   _partition(A, p, r) {
-    let x = A[r];
+    const x = A[r];
     let i = p - 1;
 
     for (let j = p; j <= r - 1; j++) {
@@ -33,11 +41,16 @@ class Quicksort {
     array[indexB] = temp;
   }
 
+  _random(min, max) {
+    let rand = min - 0.5 + Math.random() * (max - min + 1)
+    rand = Math.round(rand);
+    return rand;
+  }
+
   sort() {
     this._quicksort(this.A, 0, this.A.length - 1);
     return this.A;
   }
-
 }
 
 export default Quicksort;
